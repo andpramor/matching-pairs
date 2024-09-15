6e9890f8653f5b77e42290b38317cf5930f53654
Andres_Prado_Morgaz <andres.prado.external@eviden.com>
2024-09-16 00:21:50 +0200

    Footer spacing

--- src/shared/Footer/Footer.jsx
+++ src/shared/Footer/Footer.jsx
@@ -3,7 +3,9 @@ import './Footer.css'
 export const Footer = () => {
   return (
     <footer className='footer'>
-      <a href='https://andrespradomorgaz.com'>Andrés Prado Morgaz</a> &#169; 2024
+      <a href='https://andrespradomorgaz.com'>Andrés Prado Morgaz</a>
+      <span>&#169;</span>
+      <span>2024</span>
     </footer>
   )
 }
