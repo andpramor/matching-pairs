15f759f09a61e38a10809c5d43b68784528f2968
Andres_Prado_Morgaz <andres.prado.external@eviden.com>
2024-09-21 15:18:36 +0200

    New footer

--- src/shared/Footer/Footer.jsx
+++ src/shared/Footer/Footer.jsx
@@ -3,9 +3,13 @@ import './Footer.css'
 export const Footer = () => {
   return (
     <footer className='footer'>
-      <a href='https://andrespradomorgaz.com'>Andrés Prado Morgaz</a>
-      <span>&#169;</span>
-      <span>2024</span>
+      <span className='footerLinks'>
+        <a href='https://andrespradomorgaz.com'><i className='bi bi-person-circle' />Portfolio</a>
+        <a href='mailto:andres@andrespradomorgaz.com'><i className='bi bi-envelope-fill' />Contacto</a>
+        <a href='https://www.linkedin.com/in/apradomorgaz/'><i className='bi bi-linkedin' />LinkedIn</a>
+        <a href='https://github.com/andpramor/'><i className='bi bi-github' />Github</a>
+      </span>
+      <span>2024 &#169; Andrés Prado Morgaz</span>
     </footer>
   )
 }
